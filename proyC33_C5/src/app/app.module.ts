7c53bdf07388982e45fd453958b7422a64393479
Alejandro Jimenez <ajimenezal01@gmail.com>
2023-05-12 19:00:46 +0200

    tercer push con servicios y componente

--- proyC33_C5/src/app/app.module.ts
+++ proyC33_C5/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MicomponenteComponent } from './micomponente/micomponente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MicomponenteComponent
   ],
   imports: [
     BrowserModule
